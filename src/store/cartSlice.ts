ce84c1e9558ad93b87706aa337196387dd250156
James Serengia <jamesserengia@gmail.com>
2024-04-03 12:51:22 +0300

    Add remove item from cart functionality

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -28,11 +28,23 @@ export const cartSlice = createSlice({
       );
 
       if (itemIndex >= 0) {
-        state.items[itemIndex].quantity += 1;
+        state.items[itemIndex].quantity++;
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
-    removeFromCart() {},
+    removeFromCart(state, action: PayloadAction<{ id: string }>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        if (state.items[itemIndex].quantity === 1) {
+          state.items.splice(itemIndex, 1);
+        } else {
+          state.items[itemIndex].quantity--;
+        }
+      }
+    },
   },
 });
